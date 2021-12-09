c28e8152e279e4517032d4cba5111e51c3225335
ayushi-3536 <ayushi.sharma.3536@gmail.com>
2021-12-09 12:59:28 +0100

    - changed logger setting to debug

--- src/examples/fashion_dataset.py
+++ src/examples/fashion_dataset.py
@@ -16,10 +16,53 @@ _logger_props = {
 
 
 def input_arguments():
-    parser = default_arguments()
-    #add experiment specific arguments here
-    # parser.add_argument('--output_path', default="./fashion_logs", type=str, nargs='?',
-    #                     help='specifies the path where the results will be saved')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fix_seed', default='False', type=str, choices=['True', 'False'],
+                        nargs='?', help='seed')
+    parser.add_argument('--run_id', default=1, type=int, nargs='?',
+                        help='unique number to identify this run')
+    parser.add_argument('--runs', default=None, type=int, nargs='?', help='number of runs to perform')
+
+    parser.add_argument('--max_nodes', default=4, type=int, nargs='?',
+                        help='maximum number of nodes in the cell')
+    parser.add_argument('--gens', default=1, type=int, nargs='?',
+                        help='number of generations for DE to evolve')
+    parser.add_argument('--output_path', default="./fashion_logs", type=str, nargs='?',
+                        help='specifies the path where the results will be saved')
+    strategy_choices = ['rand1_bin', 'rand2_bin', 'rand2dir_bin', 'best1_bin', 'best2_bin',
+                        'currenttobest1_bin', 'randtobest1_bin',
+                        'rand1_exp', 'rand2_exp', 'rand2dir_exp', 'best1_exp', 'best2_exp',
+                        'currenttobest1_exp', 'randtobest1_exp']
+    parser.add_argument('--strategy', default="rand1_bin", choices=strategy_choices,
+                        type=str, nargs='?',
+                        help="specify the DE strategy from among {}".format(strategy_choices))
+    parser.add_argument('--mutation_factor', default=0.5, type=float, nargs='?',
+                        help='mutation factor value')
+    parser.add_argument('--crossover_prob', default=0.5, type=float, nargs='?',
+                        help='probability of crossover')
+    parser.add_argument('--boundary_fix_type', default='random', type=str, nargs='?',
+                        help="strategy to handle solutions outside range {'random', 'clip'}")
+    parser.add_argument('--min_budget', default=5, type=int, nargs='?',
+                        help='minimum budget')
+    parser.add_argument('--max_budget', default=25, type=int, nargs='?',
+                        help='maximum budget')
+    parser.add_argument('--eta', default=3, type=int, nargs='?',
+                        help='hyperband eta')
+    parser.add_argument('--verbose', default='False', choices=['True', 'False'], nargs='?', type=str,
+                        help='to print progress or not')
+    parser.add_argument('--folder', default=None, type=str, nargs='?',
+                        help='name of folder where files will be dumped')
+    parser.add_argument('--version', default=None, type=str, nargs='?',
+                        help='DEHB version to run')
+    parser.add_argument('--n_workers', type=int, default=1,
+                        help='Number of CPU workers for DEHB to distribute function evaluations to')
+    parser.add_argument('--runtime', type=int, default=10800,
+                        help='total runtime')
+    parser.add_argument('--log_interval', type=int, default=100,
+                        help='total runtime')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 123)')
+
     args = parser.parse_args()
     return args
 
